76a59cb35da0272ce733d2da764092b014a5c317
ultraDCR <look4rents@gmail.com>
2018-05-10 23:29:52 +0545

    Login recreated with antd

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
-import '../styles/styles.scss';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+const FormItem = Form.Item;
 
 //material-ui
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -11,53 +12,74 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import Paper from 'material-ui/Paper';
 
-const LoginPage = ({ startLogin }) => {
-  return(
-    <MuiThemeProvider>
-      <div className="box-layout">
-        <div className="box-layout__box">
-          <h1 className="box-layout__title">Look 4 Rent</h1><br/>
-        </div>
-        <Paper className="login-container" zDepth={3}>
-            <h2 className="login-title">Login </h2>
-            <TextField
-                hintText="Enter your Email"
-                floatingLabelText="Username*"
-                //onChange = {(event,newValue) => this.setState({username:newValue})}
-              /><br/>
-            <TextField
-                type="password"
-                hintText="Enter your Password"
-                floatingLabelText="Password*"
-                //onChange = {(event,newValue) => this.setState({password:newValue})}
-                />
-              <br/>
-              <RaisedButton label="Login" primary={true}
-              className="button-login"
-               //onClick={(event) => this.handleClick(event)}
-               />
-          <br/>
-          <hr/>
-          <h3>OR</h3>
-          <RaisedButton className="google-button" 
-            label="Login with Google" secondary={true}
-            onClick={startLogin}
-          /><br/><br/>
-          <RaisedButton className="facebook-button" 
-            label="Login with Facebook" primary={true}
-            onClick={startLogin}
-          />
-          </Paper>
-          
-      
-      </div>
-    </MuiThemeProvider>
+
+export class LoginPage extends Component{
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
   
-  )
-}
+
+  render(){
+    const { getFieldDecorator } = this.props.form;
+    return (
+    <MuiThemeProvider>      
+      <Paper className="login-form-container" zDepth={3}>
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <h1 className="login-form-container__title">Login</h1>
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(
+            <Checkbox>Remember me</Checkbox>
+          )}
+          <a className="login-form-forgot" href="">Forgot password</a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </FormItem>
+      </Form>
+      <hr/>
+      <h3>Login With Social Accounts</h3>
+     
+          <Button className="google-button"
+
+            onClick={startLogin}
+          >Login with Google</Button>
+            <br/><br/>
+          <Button className="facebook-button" 
+            onClick={startLogin}
+            >Login with Facebook</Button>
+      </Paper>
+    </MuiThemeProvider>
+    );
+  }            
+ }
 
 const mapDispatchToProps = (dispatch) => ({
   startLogin: () => dispatch(startLogin())
 });
+const Login = Form.create()(LoginPage);
 
-export default connect(undefined, mapDispatchToProps)(LoginPage);
+
+export default connect(undefined, mapDispatchToProps)(Login);
