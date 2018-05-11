824ce227f995897e15fcef23d04a72e0c6b73856
Deependra Dhakal <deepbhai@Deependras-MacBook-Pro.local>
2018-05-11 08:03:01 +0545

    added post ad on member area

--- src/components/DashboardMemberArea.js
+++ src/components/DashboardMemberArea.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import PostNewAd from './PostNewAd'
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
+
 
 class DashBoardMemberArea extends React.Component {
     state = {
@@ -60,13 +62,7 @@ class DashBoardMemberArea extends React.Component {
             <Layout>
               <Header style={{ background: '#fff', padding: 0 }} />
               <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                  <Breadcrumb.Item>User</Breadcrumb.Item>
-                  <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                  Bill is a cat.
-                </div>
+                <PostNewAd />
               </Content>
               <Footer style={{ textAlign: 'center' }}>
                 Ant Design ©2016 Created by Deep Bhai
